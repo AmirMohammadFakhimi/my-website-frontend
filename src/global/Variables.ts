b96931eaedf21c4f9a676c827d5701e21d716476
Amir Mohammad Fakhimi <fakhimi.amirmohamad@gmail.com>
2023-09-14 21:35:15 +0330

    Header added

---
+++ src/global/Variables.ts
@@ -0,0 +1,9 @@
+export const urls = {
+    aboutMe: 'about-me',
+    educations: 'educations',
+    experiences: 'experiences',
+    volunteering: 'volunteering',
+    projects: 'projects',
+    courses: 'courses',
+    licenses: 'licenses',
+}
