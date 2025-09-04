0b4f8889439ca9993ca34d58c30279e5a0a404b3
AmirMohammadFakhimi <fakhimi.amirmohamad@gmail.com>
2025-09-04 13:56:12 +0330

    Now we can access pages directly by entering its url

--- src/global/Storages.ts
+++ src/global/Storages.ts
@@ -7,6 +7,21 @@ export function setMenuItemUrl(menuItem: string) {
 }
 
 export function getMenuItemUrl() {
+    const currentUrl = window.location.pathname
+    let lastUrlSection: string | undefined = undefined
+    if (currentUrl && currentUrl.endsWith('/')) {
+        const urlSections = currentUrl.split('/')
+        lastUrlSection = urlSections[urlSections.length - 2]
+    } else if (currentUrl) {
+        lastUrlSection = currentUrl.split('/').pop()
+    }
+
     const menuItem = sessionStorage.getItem(menuItemsKey)
-    return menuItem ? menuItem : urls.aboutMe
+
+    if (lastUrlSection && lastUrlSection !== '' && Object.values(urls).includes(lastUrlSection))
+        return lastUrlSection
+    else if (menuItem && menuItem !== '')
+        return menuItem
+    else
+        return urls.aboutMe
 }
